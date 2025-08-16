a7dbc8dd6f77ceb12c5f38d897f22e4fbdda23b7
hayate kato <hkato@kashiwa.nagaokaut.ac.jp>
2025-08-17 01:59:52 +0900

    属性名を変更

--- dataclass/shift.py
+++ dataclass/shift.py
@@ -9,14 +9,14 @@ class Shift:
     """シフトデータを記録するクラス
     Attributes:
         summary (str): 予定の名前
-        start_dateTime (str): 予定の開始日時
-        end_dateTime (str): 予定の終了日時
-        timeZone (str): タイムゾーン
+        start_datetime (str): 予定の開始日時
+        end_datetime (str): 予定の終了日時
+        timezone (str): タイムゾーン
     """
     summary: str
-    start_dateTime: str
-    end_dateTime: str
-    timeZone: str
+    start_datetime: str
+    end_datetime: str
+    timezone: str
 
     def to_dict(self) -> dict:
         """シフト情報を辞書型に変換するメソッド
