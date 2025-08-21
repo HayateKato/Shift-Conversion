e369cf4cea3b32bfb78c115349c257ca0d3685a3
hayate kato <hkato@kashiwa.nagaokaut.ac.jp>
2025-08-21 13:28:31 +0900

    doctestを追加，メソッドの引数を変更

--- image_processor/image_processor.py
+++ image_processor/image_processor.py
@@ -4,6 +4,8 @@ from image_processor.vision_client import VisionClient
 from shift_parser import ShiftParser
 from dataclass.shift import Shift
 
+from unittest.mock import MagicMock, patch
+
 
 class ImageProcessor:
     """画像処理とシフトデータの作成を行うクラス
@@ -15,11 +17,36 @@ class ImageProcessor:
     def __init__(self):
         pass
 
-    def process_image(result_dir: str) -> list[Shift]:
+    def process_image(self, result_dir: str) -> list[Shift]:
         """画像処理とシフトデータの作成を行うメソッド
         Args:
             result_dir (str):画像や抽出結果ファイルを格納するディレクトリへのパス
         Returns:
             list[Shift]: シフトデータ
+        Examples:
+            >>> from unittest.mock import MagicMock, patch
+            >>> from dataclass.shift import Shift
+            >>>
+            >>> test_result_dir = "result/dummy"
+            >>> mock_shifts = [Shift(summary="バイト", start_datetime="2025-04-02T17:00:00+09:00", end_datetime="2025-04-02T21:30:00+09:00", timezone="Asia/Tokyo"), Shift(summary="バイト", start_datetime="2025-04-04T17:00:00+09:00", end_datetime="2025-04-04T21:00:00+09:00", timezone="Asia/Tokyo")]
+            >>>
+            >>> with patch("VisionClient") as MockClient,
+            ...      patch("ShiftParser") as MockParser:
+            ...     mock_client_instance = MockClient.return_value
+            ...     mock_client_instance.extract_data_from_image.return_value = None
+            ...     mock_parser_instance = MockParser.return_value
+            ...     mock_parser_instance.parse_data_to_shifts.return_value = mock_shifts
+            ...
+            ...     # テスト対象を実行
+            ...     test_image_processor = ImageProcessor()
+            ...     result_shifts = test_image_processor.process_image(test_result_dir)
+            ...
+            >>> # 正しい引数で呼び出されたか
+            >>> mock_client_instance.extract_data_from_image.assert_called_once_with(result_dir=test_result_dir)
+            >>> mock_parser_instance.parse_data_to_shifts.assert_called_once_with(result_dir=test_result_dir)
+            >>>
+            >>> # 返り値が正しいか
+            >>> result_shifts
+            [Shift(summary="バイト", start_datetime="2025-04-02T17:00:00+09:00", end_datetime="2025-04-02T21:30:00+09:00", timezone="Asia/Tokyo"), Shift(summary="バイト", start_datetime="2025-04-04T17:00:00+09:00", end_datetime="2025-04-04T21:00:00+09:00", timezone="Asia/Tokyo")]
         """
         pass
