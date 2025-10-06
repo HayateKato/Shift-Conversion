b077bcb8cea61fd5306eaab7513045462df76496
hayate kato <hkato@kashiwa.nagaokaut.ac.jp>
2025-10-06 16:11:28 +0900

    Dockerコンテナへの移行によるファイルパスの変更

--- image_processor/vision_client.py
+++ image_processor/vision_client.py
@@ -2,7 +2,7 @@
 
 from dotenv import load_dotenv
 import os
-load_dotenv("/Users/hayatekato/work/shift/.env")
+load_dotenv("/shift/.env")
 from google.cloud import vision
 import json
 from google.oauth2 import service_account
