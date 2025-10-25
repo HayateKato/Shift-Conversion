12b58e0cee820b7b5fdb4b92d519d8849b20c049
hayate kato <hkato@kashiwa.nagaokaut.ac.jp>
2025-10-25 16:57:07 +0900

    データベースのカラム名を変更

--- shift_web/shift_app/models.py
+++ shift_web/shift_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Image(models.Model):
-    picture = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="images/")
     title = models.CharField(max_length=200)
 
     def __str__(self):
